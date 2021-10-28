6b773a0231c539498fa1049f855ab21f8b5ed345
pphui8 <pphui8@pphui8.local>
2021-10-28 18:44:44 +0800

    update main page js

--- js/pphui8.js
+++ js/pphui8.js
@@ -1,1 +1,10 @@
-// undone
+function loaddown() {
+    var tmp = document.getElementById("in_text1");
+    tmp.style.transition = "2s ease";
+    tmp.style.transform = "none";
+    tmp.style.opacity = 1;
+    var tmp = document.getElementById("in_text2");
+    tmp.style.transition = "2s ease";
+    tmp.style.transform = "none";
+    tmp.style.opacity = 1;
+}
