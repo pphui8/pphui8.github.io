3f780e90cc8bb41318f7c7adac453bd32cd8bd37
pphui8 <55843852+pphui8@users.noreply.github.com>
2021-12-04 23:17:01 +0800

    Update pphui8.js

--- js/pphui8.js
+++ js/pphui8.js
@@ -21,4 +21,13 @@ function loaddown() {
     // 侧边栏
     var tmp = document.getElementById("side_trans");
     tmp.style.animationName = "disappre";
+
+    // egg
+    let str_base64 = "5YON44GE44Gf44KJ44CB6LKg44GR77yB";
+    let title = document.getElementById("in_text3")
+    title.title = atou(str_base64)
+}
+
+function atou(str) {
+    return decodeURIComponent(escape(window.atob(str)))
 }
